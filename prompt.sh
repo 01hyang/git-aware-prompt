e39a8056fb54e8d73c4f5952cd640fd0262d9f18
hugh <hugh_yang@castech.com.tw>
2016-10-14 19:39:52 +0800

    Add an exception list for large project which will slow down the performance

--- prompt.sh
+++ prompt.sh
@@ -1,4 +1,43 @@
+function strstr() {
+  # If compare with "", option --quiet would not work
+  # that is,
+  #     echo $1 | grep $2
+  # for
+  #     if [[ $(strstr ${curr} ${_e}) != "" ]]; then
+  echo $1 | grep --quiet $2
+}
+
+function test_git_check() {
+# local e_opt=kernel
+  local exception_list=(${e_opt} android)
+  local curr=$(pwd | tr '[:upper:]' '[:lower:]')
+
+#  curr=$(basename ${curr,,})
+#  if [[ ${exception_list[@]} =~ ${curr} ]]; then
+#    return 0
+#  else
+#    return 1
+#  fi
+
+  for _e in ${exception_list[@]}; do
+    if $(strstr ${curr} ${_e}); then
+#     echo "Match"
+      return 0
+    fi
+  done
+  return 1
+}
+
+
 find_git_branch() {
+  test_git_check
+  local skip=$?
+  if [[ skip -eq 0 ]]; then
+#   echo "Skip find_git_branch"
+    git_branch=""
+    return
+  fi
+
   # Based on: http://stackoverflow.com/a/13003854/170413
   local branch
   if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
@@ -12,6 +51,14 @@ find_git_branch() {
 }
 
 find_git_dirty() {
+  test_git_check
+  local skip=$?
+  if [[ skip -eq 0 ]]; then
+#   echo "Skip find_git_dirty"
+    git_dirty=''
+    return
+  fi
+
   local status=$(git status --porcelain 2> /dev/null)
   if [[ "$status" != "" ]]; then
     git_dirty='*'
